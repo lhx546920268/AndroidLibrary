81d4cd43c095590a96eb9979dd76bed66e48a9d6
Hank <Hank@aizhongjiedeMacBook-Air.local>
2017-08-15 11:29:20 +0800

    更新代码

--- library/src/main/java/com/lhx/library/util/AppUtil.java
+++ library/src/main/java/com/lhx/library/util/AppUtil.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.annotation.DrawableRes;
@@ -11,8 +12,12 @@ import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
 import com.lhx.library.dialog.AlertController;
+
+import java.util.Locale;
+import com.lhx.library.util.ToastUtil;
 
 /**
  * app有关的工具类
@@ -133,6 +138,63 @@ public class AppUtil {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     *
+     * @param dest 目的地
+     * @param destLatitude 目的地维度
+     * @param destLongitude 目的地经度
+     */
+    public static void openMapForNavigation(final Context context, final String dest, final double destLatitude, final double destLongitude){
+
+        AlertController controller = AlertController.buildActionSheet(context, "查看路线", "百度地图", "高德地图");
+        controller.setOnItemClickListener(new AlertController.OnItemClickListener() {
+            @Override
+            public void onItemClick(AlertController controller, int index) {
+                switch (index){
+                    case 0 : {
+                        if(isInstallByread(context, "com.baidu.BaiduMap")){
+                            Intent intent = new Intent();
+                            intent.setData(Uri.parse(String.format(Locale.CHINA,
+                                    ("baidumap://map/direction?origin={{我的位置}}&destination=latlng:%f," +
+                                            "%f|name=%s&mode=driving&coord_type=gcj02"), destLatitude, destLongitude, dest)));
+                            context.startActivity(intent);
+                        }else {
+                            Toast.makeText(context, "没有安装百度地图", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    break;
+                    case 1 : {
+                        if(isInstallByread(context, "com.autonavi.minimap")){
+                            Intent intent = new Intent();
+                            intent.setData(Uri.parse(String.format(Locale.CHINA,
+                                    "androidamap://navi?sourceApplication= &backScheme= &lat=%f&lon=%f&dev=0&style=2",
+                                    destLatitude, destLongitude)));
+                            context.startActivity(intent);
+                        }else {
+                            Toast.makeText(context, "没有安装高德地图", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                    break;
+                }
+            }
+        });
+
+        controller.show();
+    }
+
+    //判断是否安装了某个应用
+    public static boolean isInstallByread(Context context, String packageName) {
+        PackageInfo packageInfo;
+        try{
+            packageInfo = context.getPackageManager().getPackageInfo(packageName, 0);
+        }catch (PackageManager.NameNotFoundException e){
+            packageInfo = null;
+        }
+
+        return packageInfo != null;
     }
 
 }
